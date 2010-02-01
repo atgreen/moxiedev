2511bbb347ec9e0131f13c5c33eee9afac424cf7
Anthony Green <green@moxlog.(none)>
2010-02-01 02:13:03 -0500

    More RPM build fixes.

--- scripts/mkrpms.sh
+++ scripts/mkrpms.sh
@@ -4,9 +4,9 @@
 # local yum repo at $REPO.
 
 #CPU=x86_64
-CPU=i386
+CPU=i686
 
-TARGET=fedora-12-$CPU
+TARGET=fedora-12-i386
 RESULTDIR=/var/lib/mock/$TARGET/result
 REPO=dist/MoxieLogic/$CPU
 
@@ -17,29 +17,30 @@ createrepo $REPO
 
 mock -r $TARGET dist/moxie-elf-binutils-*src.rpm
 mv $RESULTDIR/moxie-elf-binutils-debuginfo*.rpm $REPO/RPMS/$CPU/debuginfo
-mv $RESULTDIR/moxie-elf-binutils-*.rpm $REPO/RPMS/$CPU
 mv $RESULTDIR/moxie-elf-binutils-*src.rpm  $REPO/SRPMS
+mv $RESULTDIR/moxie-elf-binutils-*.rpm $REPO/RPMS/$CPU
 createrepo $REPO
 
 mock -r $TARGET dist/moxie-elf-gcc-*src.rpm
 mv $RESULTDIR/moxie-elf-gcc-debuginfo*.rpm $REPO/RPMS/$CPU/debuginfo
-mv $RESULTDIR/moxie-elf-gcc-*.rpm $REPO/RPMS/$CPU
 mv $RESULTDIR/moxie-elf-gcc-*src.rpm  $REPO/SRPMS
+mv $RESULTDIR/moxie-elf-gcc-*.rpm $REPO/RPMS/$CPU
 createrepo $REPO
 
 mock -r $TARGET dist/moxie-elf-newlib-*src.rpm
-mv $RESULTDIR/moxie-elf-newlib-*noarch.rpm $REPO/RPMS/noarch
+mv $RESULTDIR/moxie-elf-newlib-debuginfo*.rpm $REPO/RPMS/$CPU/debuginfo
 mv $RESULTDIR/moxie-elf-newlib-*src.rpm  $REPO/SRPMS
+mv $RESULTDIR/moxie-elf-newlib-*.rpm $REPO/RPMS/$CPU
 createrepo $REPO
 
 mock -r $TARGET dist/moxie-elf-gdb-*src.rpm
 mv $RESULTDIR/moxie-elf-gdb-debuginfo*.rpm $REPO/RPMS/$CPU/debuginfo
-mv $RESULTDIR/moxie-elf-gdb-*.rpm $REPO/RPMS/$CPU
 mv $RESULTDIR/moxie-elf-gdb-*src.rpm  $REPO/SRPMS
+mv $RESULTDIR/moxie-elf-gdb-*.rpm $REPO/RPMS/$CPU
 createrepo $REPO
 
 mock -r $TARGET dist/moxie-elf-qemu-*src.rpm
 mv $RESULTDIR/moxie-elf-qemu-debuginfo*.rpm $REPO/RPMS/$CPU/debuginfo
-mv $RESULTDIR/moxie-elf-qemu-*.rpm $REPO/RPMS/$CPU
 mv $RESULTDIR/moxie-elf-qemu-*src.rpm  $REPO/SRPMS
+mv $RESULTDIR/moxie-elf-qemu-*.rpm $REPO/RPMS/$CPU
 createrepo $REPO
